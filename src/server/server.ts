7bd28218eec0dccd1da290fb9cb61fa307d1f1c0
irines <irina.kiselyowa12@gmail.com>
2023-03-14 18:09:20 +0200

    adding EJS templating lang to the server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,19 @@
-import express from 'express'
+import express from "express";
 
-console.log(express)
+const server = express();
+
+// middleware
+server.use(express.static("dist"));
+
+server.set("view engine", "ejs");
+
+// Variables can be passed via res.render 2nd parameter. It will be used to injecr React into HTML template
+server.use("/", (req, res) => {
+    res.render("index", {
+        content: 'EJS is cool'
+    })
+})
+
+server.listen("8080", "localhost", () => {
+  console.info("Server is listening at http://localhost:8080");
+});
