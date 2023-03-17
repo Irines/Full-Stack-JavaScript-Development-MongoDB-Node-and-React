3ca6d31b908a24ba1c90094b361e4561860eeeb0
irines <irina.kiselyowa12@gmail.com>
2023-03-17 21:23:50 +0200

    fetch data frpm MongoDB

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,14 +1,35 @@
 import express from "express";
-import testData from "../test-data.json";
+// import testData from "../test-data.json";
 import cors from "cors";
-
+import { connectClient } from "./db";
 
 const router = express.Router();
-router.use(cors())
+router.use(cors());
 
-router.get("/contests", (req, res) => {
+router.get("/contests", async (req, res) => {
   // get data from the MongoDB
-  res.send({contests: testData});
+  const client = await connectClient();
+  const contests = await client.collection("contests")
+  .find()
+  .project({
+    id: 1,
+    categoryName: 1,
+    contestName: 1,
+    _id: 0,
+  })
+  .toArray();
+  res.send({ contests });
 });
+
+router.get("/contest/:contestId", async (req, res) => {
+  // get data from the MongoDB
+  const client = await connectClient();
+  const contest = await client.collection("contests")
+  .findOne({
+    "id": req.params.contestId
+  })
+  res.send({ contest });
+});
+
 
 export default router;
