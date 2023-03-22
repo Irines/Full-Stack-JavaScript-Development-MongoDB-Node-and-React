f3263c0253ac929365fd9670017424c42f1b7471
irines <irina.kiselyowa12@gmail.com>
2023-03-22 14:43:37 +0200

    handle form submit via DOM API

--- src/components/contest.tsx
+++ src/components/contest.tsx
@@ -24,28 +24,44 @@ const Contest = ({ initialContest }) => {
         window.history.pushState({}, "", `/`);
     }
 
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const newName = event.target.newName;
+        console.log(newName.value);
+    }
+
     return (
         <>
             <Header message={currentContest?.contestName} />
             <div className="contest">
                 <div className="title">Contest Description</div>
                 <div className="description">{currentContest?.description}</div>
+
                 <div className="title">Proposed Names</div>
                 <div className="body">
                     {
-                        currentContest?.names?.length 
-                        ? 
-                        (
-                            <div className="list">
-                                {currentContest.names.map((nameObj) => (
-                                    <div key={nameObj.id} className="item">{nameObj.name}</div>
-                                ))}
-                            </div>
-                        )
-                        :
-                        <div>No names proposed yet. </div>
+                        currentContest?.names?.length
+                            ?
+                            (
+                                <div className="list">
+                                    {currentContest.names.map((nameObj) => (
+                                        <div key={nameObj.id} className="item">{nameObj.name}</div>
+                                    ))}
+                                </div>
+                            )
+                            :
+                            <div>No names proposed yet. </div>
                     }
                 </div>
+
+                <div className="title">Propose a New Name</div>
+                <div className="body">
+                    <form onSubmit={handleFormSubmit}>
+                        <input type="text" name="newName" />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+
                 <a href="/" className="link" onClick={navigateToContestList}>Contest List</a>
             </div>
         </>
